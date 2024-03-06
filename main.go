92cb930c559d6f55e92158e74c661b52bf68b4bd
Dikontay <dalimbayeva@icloud.com>
2024-03-06 18:15:51 +0500

    add dependencies

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"auth/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+	initializers.SyncDatabase()
+}
+func main() {
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
